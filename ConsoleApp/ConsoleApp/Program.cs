6e10e73759c1b08cb157d8fefded4cb3e5664b10
jjabid@cascadeo.com <jjabid@cascadeo.com>
2024-01-03 19:57:22 +0800

    Added time

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World! Test 2 Jan 3 2024");
+        Console.WriteLine("Hello, World! Test 2 Jan 3 2024 Test 2 7:58 PM");
     }
 }
 
